f380f675e0a17df2ffe3e0095d122bcd401bc2c9
Mark Klaiman <markkl1998@gmail.com>
2023-01-31 17:35:45 +0200

    X-means added

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 import json
 from compare_clustering_solutions import evaluate_clustering
+import numpy as np
+from sklearn.cluster import KMeans
 
+
+def x_means(data, k_min, k_max):
+    best_k = k_min
+    best_inertia = float('inf')
+
+    for k in range(k_min, k_max + 1):
+        kmeans = KMeans(n_clusters=k)
+        kmeans.fit(data)
+
+        if kmeans.inertia_ < best_inertia:
+            best_k = k
+            best_inertia = kmeans.inertia_
+            best_kmeans = kmeans
+
+    return best_kmeans, best_k
+
+
+# # Example usage
+# X = np.array([[1, 2], [1, 4], [1, 0], [4, 2], [4, 4], [4, 0]])
+# k_min = 2
+# k_max = 5
+# kmeans = x_means(X, k_min, k_max)
+# print(kmeans.labels_)
 
 def analyze_unrecognized_requests(data_file, output_file, num_rep, min_size):
     # todo: implement this function
@@ -8,7 +33,7 @@ def analyze_unrecognized_requests(data_file, output_file, num_rep, min_size):
     #  but don't split your code into multiple *.py files
     #
     #  todo: the final outcome is the json file with clustering results saved as output_file
-
+    print(data_file, output_file, num_rep, min_size)
     pass
 
 
